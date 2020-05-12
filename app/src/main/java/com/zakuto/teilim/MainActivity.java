237fd4f9cb1f69bb298b5b52f8ee974d40024808
nisozakuto <nisozakuto@gmail.com>
2020-05-12 18:10:33 -0400

    First Commit

---
+++ app/src/main/java/com/zakuto/teilim/MainActivity.java
@@ -0,0 +1,65 @@
+package com.zakuto.teilim;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    public Button tumKitap,infoButton;
+public TextView teilimText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        tumKitap =findViewById(R.id.tumKitapButton);
+        tumKitap.setOnClickListener(this);
+
+        infoButton = findViewById(R.id.infoButton);
+        infoButton.setOnClickListener(this);
+
+        teilimText = findViewById(R.id.textView2);
+        teilimText.setText(R.string.dummmyText);
+    }
+
+    
+    public void tumKitap() {
+        Intent tumKitapIntent = new Intent(this, tumKitap.class);
+        startActivity(tumKitapIntent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tumKitapButton:
+                tumKitap();
+                break;
+            case R.id.infoButton:
+                Intent infoIntent = new Intent(this, Info.class);
+                startActivity(infoIntent);
+        }
+    }
+}
---
+++ app/src/main/java/com/zakuto/teilim/tumKitap.java
@@ -0,0 +1,58 @@
+package com.zakuto.teilim;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class tumKitap extends AppCompatActivity {
+    public TextView printTeilim;
+ListView teilimListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tum_kitap);
+
+        String teilim1 = getResources().getString(R.string.perek1);
+        String teilim2 = getResources().getString(R.string.perek2);
+        String teilim3 = getResources().getString(R.string.perek3);
+        String teilim4 = getResources().getString(R.string.perek4);
+        String teilim5 = getResources().getString(R.string.perek5);
+        String teilim6 = getResources().getString(R.string.perek6);
+        String teilim7 = getResources().getString(R.string.perek7);
+        String teilim8 = getResources().getString(R.string.perek8);
+        String teilim9 = getResources().getString(R.string.perek9);
+        String teilim10 = getResources().getString(R.string.perek10);
+
+        String array1[] = {teilim1,
+                teilim2,
+                teilim3,
+                teilim4,
+                teilim5,
+                teilim6,
+                teilim7,
+                teilim8,
+                teilim9,
+                teilim10};
+
+        teilimListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.content_tum_kitap, R.id.textViewForTeilim, array1);
+        teilimListView.setAdapter(arrayAdapter);
+
+
+
+        // printTeilim = findViewById(R.id.printTeilim);
+       // printTeilim.setText(R.stritextViewForTeilimng.perek1);
+       // printTeilim.append(printTeilim + getResources().getString(R.string.perek2));
+    }
+
+}
