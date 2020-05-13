68446a2f88996e189f082d335361aa3008be0481
Onur Ergalimidi <39571524+ergalimidi@users.noreply.github.com>
2020-05-14 00:34:33 +0300

    Teilim 11
    Teilim 11

--- app/src/main/java/zakuto/tehilimtr/tumKitap.java
+++ app/src/main/java/zakuto/tehilimtr/tumKitap.java
@@ -29,6 +29,7 @@ ListView tehilimtrListView;
         String tehilimtr8 = getResources().getString(R.string.perek8);
         String tehilimtr9 = getResources().getString(R.string.perek9);
         String tehilimtr10 = getResources().getString(R.string.perek10);
+        String teilimtr11 = getResources().getSring(R.string.perek11);
 
         String array1[] = {tehilimtr1,
                 tehilimtr2,
