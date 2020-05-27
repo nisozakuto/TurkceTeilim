c0baf5e28b14852da72e85c095f6b34db4797111
nisozakuto <nisozakuto@gmail.com>
2020-05-27 16:27:06 -0400

    Listview adapters are working properly

--- app/src/main/java/zakuto/tehilimtr/FullBookFragment.java
+++ app/src/main/java/zakuto/tehilimtr/FullBookFragment.java
@@ -20,7 +20,6 @@ public class FullBookFragment extends Fragment {
 
     private MonthlyViewModel mViewModel;
     int[] passTehilimValues = new int[]{};
-    String[] monthlyTehilimArray = {"1.Kitap 1-9", "2.Kitap 10-17", "3.Kitap 18-22", "4.Kitap 23-28", "5.Kitap 29-34", "6.Kitap 35-38", "7.Kitap 39-43", "8.Kitap 44-48", "9.Kitap 49-54", "10.Kitap 55-59", "11.Kitap 60-65", "12.Kitap 66-68", "13.Kitap 69-71", "14.Kitap 72-76", "15.Kitap 77-78", "16.Kitap 79-82", "17.Kitap 83-87", "18.Kitap 88-89", "19.Kitap 90-96", "20.Kitap 97-103", "21.Kitap 104-105", "22.Kitap 106-107", "23.Kitap 108-112", "24.Kitap 113-118", "25.Kitap 119-1-96", "26.Kitap 119-97-176", "27.Kitap 120-134", "28.Kitap 135-139", "29.Kitap 140-144", "30.Kitap 145-150"};
     String[] tehilimArray = new String[150];
 
     public static FullBookFragment newInstance() {
@@ -33,17 +32,17 @@ public class FullBookFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_full_book, container, false);
 
         for (int i = 1; i < 151; i++) {
-            tehilimArray[i - 1] = i + ".Gün";
+            tehilimArray[i-1] = i + ".Gün";
         }
 
-        ListView monthlyListView = (ListView) view.findViewById(R.id.monthlyFragmentListView);
+        ListView fullBookListView = (ListView) view.findViewById(R.id.monthlyFragmentListView);
         ArrayAdapter<String> listviewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tehilimArray);
-        monthlyListView.setAdapter(listviewAdapter);
-        monthlyListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        fullBookListView.setAdapter(listviewAdapter);
+        fullBookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
                 position += 1;
-                Toast.makeText(getActivity(), "Gün " + position + " açılıyor!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Kitap " + position + " açılıyor!", Toast.LENGTH_SHORT).show();
                 FragmentTransaction fr = getChildFragmentManager().beginTransaction();
                 fr.replace(R.id.monthlyFragmentId, new readFragment());
                 fr.commit();
